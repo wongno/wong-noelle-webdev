4b5277dd12a45c0b67b7e7acf0ba3631825cd64b
wongno <noellegwong@gmail.com>
2017-07-12 00:52:36 -0400

    fixing again

--- test/app.js
+++ test/app.js
@@ -6,13 +6,12 @@ app.post("/api/test", createMessage);
 app.delete("/api/test/:id", deleteMessage);
 
 var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-console.log("SOOOOOOOOOOOOOMETHING!!!!");
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-console.log("SOMETHING!!!!!!!!!!!!!!!");    
-var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely 
+ var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds153652.mlab.com:53652/heroku_g56x6dg6'; // user yours
+    connectionString += '@ds153732.mlab.com:53732/heroku_863lxjmk'; // user yours
 }
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
 // above with your own URL given to you by mLab
