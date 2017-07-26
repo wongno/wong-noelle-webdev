bfb5a3e9e17cdddc2ae84073735f6dd83ec0e410
wongno <noellegwong@gmail.com>
2017-07-25 22:24:28 -0400

    enabled creating and editing widgets

--- public/assignment/views/widget/controllers/widget-edit.controller.client.js
+++ public/assignment/views/widget/controllers/widget-edit.controller.client.js
@@ -4,11 +4,27 @@
         .controller("EditWidgetController", EditWidgetController);
 
     function EditWidgetController(WidgetService, $routeParams) {
+        var model = this;
 
+        model.widgetId = $routeParams["widgetId"];
+        model.pageId = $routeParams.pageId;
+        model.websiteId = $routeParams.websiteId;
+        model.userId = $routeParams.userId;
+        model.updateWidget = updateWidget;
+        model.deleteWidget = deleteWidget;
         function init() {
-
+            model.widgets = WidgetService.findWidgetsByPageId(model.pageId);
+            model.widget = WidgetService.findWidgetById(model.widgetId);
         }
 
         init();
+
+        function updateWidget(widget){
+            WidgetService.updateWidget(model.widgetId,widget);
+        }
+
+        function deleteWidget() {
+            WidgetService.deleteWidget(model.widgetId);
+        }
     }
 })();
--- public/assignment/views/widget/controllers/widget-new.controller.client.js
+++ public/assignment/views/widget/controllers/widget-new.controller.client.js
@@ -4,10 +4,34 @@
         .controller("NewWidgetController", NewWidgetController);
 
     function NewWidgetController(WidgetService, $routeParams) {
-
+        var model = this;
+        model.pageId = $routeParams.pageId;
+        model.websiteId = $routeParams.websiteId;
+        model.userId = $routeParams.userId;
+        model.createWidget = createWidget;
+        model.setWidgetHeading = setWidgetHeading;
+        model.setWidgetImage = setWidgetImage;
+        model.setWidgetYoutube = setWidgetYoutube;
+        model.widget = model.createWidget();
         function init () {
-
+            model.widgets = WidgetService.findWidgetsByPageId(model.pageId);
         }
         init();
+
+
+        function setWidgetHeading(){
+            model.widget.widgetType = "HEADING";
+        }
+        function setWidgetImage(){
+            model.widget.widgetType = "IMAGE";
+        }
+        function setWidgetYoutube(){
+            model.widget.widgetType = "YOUTUBE";
+        }
+        function createWidget() {
+            var widget = Object();
+            widget.widgetType = null;
+            return WidgetService.createWidget(model.pageId, widget);
+        }
     }
 })();
