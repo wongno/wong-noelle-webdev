d8b2c346bf66cbc7ce6141d738b205345949c8bf
wongno <noellegwong@gmail.com>
2017-07-28 23:35:56 -0400

    added Uml file

--- public/assignment/views/user/controllers/login.controller.client.js
+++ public/assignment/views/user/controllers/login.controller.client.js
@@ -4,7 +4,7 @@
         .module("WebAppMaker")
         .controller("LoginController", LoginController);
 
-    function LoginController($location, UserService) {
+    function LoginController($location, UserService, $rootScope) {
         var model = this;
 
         model.login = login;
@@ -23,6 +23,7 @@
             if(user === null) {
                 model.errorMessage = "User not found";
             } else {
+
                 $location.url("profile/"+user._id);
             }
         }
