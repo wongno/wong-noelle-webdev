b5dd9aaca7bd5a6e1a03afe6b5bcd0af35d74b24
wongno <noellegwong@gmail.com>
2017-08-07 19:32:48 -0400

    refractored createWebsite

--- assignment/model/user/user.model.server.js
+++ assignment/model/user/user.model.server.js
@@ -5,6 +5,16 @@ var userModel = mongoose.model("UserModel", userSchema);
 module.exports = userModel;
 
 userModel.createUser = createUser;
+userModel.addWebsite = addWebsite;
+
+function addWebsite(developerId, websiteId) {
+    userModel
+        .findById(developerId)
+        .then(function (user) {
+            user.websites.push(websiteId);
+            return user.save();
+        });
+}
 
 function createUser(user) {
     return userModel.create(user);
--- assignment/model/website/website.model.server.js
+++ assignment/model/website/website.model.server.js
@@ -1,13 +1,25 @@
 var mongoose = require("mongoose");
 var websiteSchema = require("./website.schema.server");
 var websiteModel = mongoose.model("WebsiteModel", websiteSchema);
+var userModel = require('../user/user.model.server');
 
 websiteModel.createWebsite = createWebsite;
+websiteModel.findWebsitesByUser = findWebsiteByUser;
 module.exports = websiteModel;
+module.exports = userModel;
 
 function createWebsite(developerId, website){
     website.developer = developerId;
-    return websiteModel.create(website);
+    var websiteTmp = null''
+    return websiteModel
+        .create(website)
+        .then(function (websiteDoc) {
+            websiteTmp = websiteDoc;
+            return userModel.addWebsite(developerId, websiteDoc);
+        })
+        .then(function (userDoc) {
+            return websiteTmp;
+        })
 }
 
 function findWebsiteByUser(developerId) {
