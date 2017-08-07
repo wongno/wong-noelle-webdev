0d88aac4c241e01bc5dd3d106693efdfbc4c0fdd
wongno <noellegwong@gmail.com>
2017-08-07 19:00:32 -0400

    added createWebsite to website Model

--- assignment/model/website/website.model.server.js
+++ assignment/model/website/website.model.server.js
@@ -2,4 +2,10 @@ var mongoose = require("mongoose");
 var websiteSchema = require("./website.schema.server");
 var websiteModel = mongoose.model("WebsiteModel", websiteSchema);
 
+websiteModel.createWebsite = createWebsite;
 module.exports = websiteModel;
+
+function createWebsite(developerId, website){
+    website.developer = developerId;
+    return websiteModel.create(website);
+}
--- assignment/model/website/website.schema.server.js
+++ assignment/model/website/website.schema.server.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var websiteSchema = mongoose.Schema({
     name: String,
     description: String,
+    //role : {type: String, enum:["ADMIN", "STUDENT"]};
     developer: {type: mongoose.Schema.Types.ObjectId, ref:"UserModel"},
     created: {type: Date, default: Date.now}
 }, {collection:"website"});
