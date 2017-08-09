340a258c603226511c2efa4ecbc3f3374189b4bc
wongno <noellegwong@gmail.com>
2017-08-08 21:14:07 -0400

    added information to pageSchema and pageModel

--- assignment/model/page/page.schema.server.js
+++ assignment/model/page/page.schema.server.js
@@ -1,12 +1,10 @@
 var mongoose = require("mongoose");
 var pageSchema = mongoose.Schema({
-    username: String,
-    password: String,
-    firstName: String,
-    lastName: String,
-    email: String,
-    phone: String,
-    websites: [{type: mongoose.Schema.Types.ObjectId, ref:"WebsiteModel"}],
+    _website: {type: mongoose.Schema.Types.ObjectId, ref:"WebsiteModel"},
+    name: String,
+    title: String,
+    description: String,
+    widgets: [{type: mongoose.Schema.Types.ObjectId, ref:"WidgetModel"}],
     dateCreated: {type: Date, default: Date.now},
-}, {collection:"user"});
+}, {collection:"page"});
 module.exports = pageSchema;
--- assignment/model/website/website.schema.server.js
+++ assignment/model/website/website.schema.server.js
@@ -1,28 +1,9 @@
 var mongoose = require('mongoose');
 var websiteSchema = mongoose.Schema({
-    //
-    // _user
-    // Reference to User
-    // Refers to parent user
-    // name
-    // String
-    //
-    //
-    // description
-    // String
-    //
-    //
-    // pages
-    //     [Page]
-    // Array of references to child page instances
-    // dateCreated
-    // Date
-    // Current date
     _user: {type: mongoose.Schema.Types.ObjectId, ref:"UserModel"},
     name: String,
     description: String,
     pages: [{type: mongoose.Schema.Types.ObjectId, ref:"PagesModel"}],
-    //role : {type: String, enum:["ADMIN", "STUDENT"]};
     created: {type: Date, default: Date.now}
 }, {collection:"website"});
 module.exports = websiteSchema;
