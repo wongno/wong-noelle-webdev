1e4f8a3f24e4b7a7410749e34f7186df01ca976d
wongno <noellegwong@gmail.com>
2017-08-14 11:20:06 -0400

    added shelter-profile and adopter-profeile views and controllers

--- public/project/config.js
+++ public/project/config.js
@@ -19,16 +19,26 @@
                 controller: "RegisterController",
                 controllerAs: "model"
             })
+            .when("/adopter/profile/:adopterId", {
+                templateUrl: "views/user/templates/adopter-profile.view.client.html",
+                controller: "AdopterProfileController",
+                controllerAs: "model"
+            })
+            .when("/shelter/profile/:shelterId", {
+                templateUrl: "views/user/templates/shelter-profile.view.client.html",
+                controller: "ShelterProfileController",
+                controllerAs: "model"
+            })
             .when("/pet/:petId/profile", {
                 templateUrl: "views/user/anonymous/templates/detailed-animal-profile.view.client.html",
                 controller: "AnimalSearchController",
                 controllerAs: "model"
             })
-            .when("/shelter/:shelterId/profile", {
-                templateUrl: "views/user/shelter/templates/shelter-profile.view.client.html",
-                controller: "ShelterProfileController",
-                controllerAs: "model"
-            })
+            // .when("/shelter/:shelterId/profile", {
+            //     templateUrl: "views/user/shelter/templates/shelter-profile.view.client.html",
+            //     controller: "ShelterProfileController",
+            //     controllerAs: "model"
+            // })
 
     }
 })();
---
+++ public/project/views/user/controllers/adopter-profile.controller.client.js
@@ -0,0 +1,31 @@
+(function () {
+
+    angular
+        .module("PetAppMaker")
+        .controller("AdopterProfileController", AdopterProfileController);
+
+    function AdopterProfileController($routeParams, UserService, $location) {
+        var model = this;
+        var userId = $routeParams["userId"];
+
+        model.updateUser = updateUser;
+        model.deleteUser = deleteUser;
+
+        function init() {
+            UserService.findUserById(userId)
+                .then(function (response) {
+                    model.user = response.data;
+                });
+        }
+        init();
+
+        function updateUser(user) {
+            UserService.updateUser(user._id, user);
+        }
+
+        function deleteUser() {
+            UserService.deleteUser(userId);
+        }
+    }
+
+})();
